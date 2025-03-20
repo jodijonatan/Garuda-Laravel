55db8cc65d5809c9733b63ae0ef111a139cde097
jodijonatan <jdi102350@gmail.com>
2025-03-20 19:30:18 +0700

    add flight

--- app/Models/FlightClass.php
+++ app/Models/FlightClass.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FlightClass extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'flight_id',
+        'class_type',
+        'price',
+        'total_seats'
+    ];
 }
