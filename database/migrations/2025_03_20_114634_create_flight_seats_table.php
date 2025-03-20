53449819df38b8ce8010cafff9e2e9ad26cacfeb
jodijonatan <jdi102350@gmail.com>
2025-03-20 19:38:39 +0700

    add sealt

--- database/migrations/2025_03_20_114634_create_flight_seats_table.php
+++ database/migrations/2025_03_20_114634_create_flight_seats_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('flight_seats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('flight_id')->constrained()->cascadeOnDelete();
+            $table->string('row');
+            $table->string('column');
+            $table->enum('class_type', ['economy', 'bussiness']);
+            $table->boolean('is_available');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
