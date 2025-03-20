afbe094eb8bcc8a26ebbba238669770fed244714
jodijonatan <jdi102350@gmail.com>
2025-03-20 20:24:49 +0700

    fix of migrate

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Facilty extends Model
+class Facility extends Model
 {
     use HasFactory, SoftDeletes;
 
