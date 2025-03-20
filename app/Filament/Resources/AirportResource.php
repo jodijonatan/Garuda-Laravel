3239aeeabc76262832fc1a0cb13c3076c5471c9d
jodijonatan <jdi102350@gmail.com>
2025-03-20 22:05:53 +0700

    lanjut besok lur

---
+++ app/Filament/Resources/AirportResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AirportResource\Pages;
+use App\Filament\Resources\AirportResource\RelationManagers;
+use App\Models\Airport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AirportResource extends Resource
+{
+    protected static ?string $model = Airport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image')->image()->directory('airports')->required()->columnSpan(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAirports::route('/'),
+            'create' => Pages\CreateAirport::route('/create'),
+            'edit' => Pages\EditAirport::route('/{record}/edit'),
+        ];
+    }
+}
