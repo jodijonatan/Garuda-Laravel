6dae0e6432a84bc0912cabf692daacb5150437b2
jodijonatan <jdi102350@gmail.com>
2025-03-22 09:45:12 +0700

    add facilty

---
+++ app/Filament/Resources/PromoCodeResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PromoCodeResource\Pages;
+use App\Filament\Resources\PromoCodeResource\RelationManagers;
+use App\Models\PromoCode;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PromoCodeResource extends Resource
+{
+    protected static ?string $model = PromoCode::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('code')
+                ->required(),
+                Forms\Components\Select::make('discount_type')
+                ->required()
+                ->options([
+                    'fixed' => 'Fixed',
+                    'percentage' => 'Percentage'
+                ]),
+                Forms\Components\TextInput::make('discount')
+                ->required()
+                ->numeric()
+                ->minValue(0),
+                Forms\Components\DateTimePicker::make('valid_until')
+                ->required(),
+                Forms\Components\Toggle::make('is_used')
+                ->required(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPromoCodes::route('/'),
+            'create' => Pages\CreatePromoCode::route('/create'),
+            'edit' => Pages\EditPromoCode::route('/{record}/edit'),
+        ];
+    }
+}
