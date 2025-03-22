7877be2acf637d1180bf6dcbdaa42f99bc306938
jodijonatan <jdi102350@gmail.com>
2025-03-22 14:45:44 +0700

    add facilities in flight

--- app/Filament/Resources/FlightResource.php
+++ app/Filament/Resources/FlightResource.php
@@ -11,6 +11,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\Relationship;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class FlightResource extends Resource
@@ -26,15 +27,57 @@ class FlightResource extends Resource
                 Forms\Components\Wizard::make([
                     Forms\Components\Wizard\Step::make('Flight Information')
                 ->schema([
-
+                    Forms\Components\TextInput::make('flight_number')
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(255),
+                    Forms\Components\Select::make('airline_id')
+                    ->relationship('airline', 'name')
+                    ->required(),
                 ]),
                 Forms\Components\Wizard\Step::make('Flight Segments')
                 ->schema([
-
+                    Forms\Components\Repeater::make('flight_segment')
+                    ->relationship('segments')
+                    ->schema([
+                        Forms\Components\TextInput::make('sequence')
+                        ->numeric()
+                        ->required(),
+                        Forms\Components\Select::make('airport_id')
+                        ->relationship('airport', 'name')
+                        ->required(),
+                        Forms\Components\DateTimePicker::make('time')
+                        ->required(),
+                    ])
+                    ->collapsed(false)
+                    ->minItems(1),
                 ]),
                 Forms\Components\Wizard\Step::make('Flight Class')
                 ->schema([
-
+                    Forms\Components\Repeater::make('flight_classes')
+                    ->relationship('classes')
+                    ->schema([
+                        Forms\Components\Select::make('class_type')
+                        ->options([
+                            'economy' => 'Economy',
+                            'bussiness' => 'Bussiness',
+                        ])
+                        ->required(),
+                        Forms\Components\TextInput::make('price')
+                        ->required()
+                        ->prefix('IRD')
+                        ->numeric()
+                        ->minValue(0),
+                        Forms\Components\TextInput::make('total_seats')
+                        ->required()
+                        ->numeric()
+                        ->minValue(1)
+                        ->label('Total Seats'),
+                        Forms\Components\Select::make('facilities')
+                        ->relationship('facilities', 'name')
+                        ->multiple()
+                        ->required(),
+                    ])
                 ]),
             ])->columnSpan(2)
         ]);
